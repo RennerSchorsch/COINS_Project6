cd51e4fd4fb7b91c737302b04a2f5adc91d0b5cc
RennerSchorsch <g.s.renner@web.de>
2014-05-12 01:44:58 +0200

    Starting to implement the cli score

---
+++ ComplexityCalculator/src/main/java/com/coinsproject6/complexitycalculator/cli/CLIScore.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.coinsproject6.complexitycalculator.cli;
+
+import com.coinsproject6.complexitycalculator.text.Text;
+
+/**
+ *
+ * @author Georg
+ */
+public class CLIScore {
+    
+    private double cliScore;
+    private double avgLetters;
+    private double avgSentence;
+    
+    public CLIScore(Text textStucture){
+        
+        this.avgLetters = textStucture.getAvgLetters();
+        this.avgSentence = textStucture.getAvgSentence();
+        setCliScore();
+    }
+    
+    private void setCliScore(){
+        cliScore = 0.0588 * avgLetters - 0.296 * avgSentence - 15.8;
+    }
+    
+    public double getCLIScore(){
+        return cliScore;
+    }
+}
---
+++ ComplexityCalculator/src/main/java/com/coinsproject6/complexitycalculator/text/Text.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.coinsproject6.complexitycalculator.text;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Komplexe Datenstruktur, die den Text mit allen nötigen Informationen repräsentiert.
+ * 
+ * @author Georg
+ */
+public class Text {
+    
+    private String text;
+    private Map<String, Integer> textInfo;
+    private Integer amountWords;
+    private Integer amountSentence;
+    private double avgLetters;
+    private double avgSentence;
+    
+    public Text(String text){
+        this.text = text;
+        setTextInfo(text);
+        setAmountWords(text);
+        
+    }
+    
+    public void setTextInfo(String text){        
+        textInfo = null;
+    }
+    
+    public Map<String, Integer> getTextInfo(){
+    
+    return textInfo;
+    }
+    
+    public void setAmountWords(String text){   
+        amountWords = 0;
+    }
+    
+    public Integer getAmountWords(){
+        
+        return amountWords;
+    }
+    
+    public void setAmountSentence(String text){
+        amountSentence = 0;
+    }
+    
+    public Integer getAmountSentence(){
+        
+        return amountSentence;
+    }
+    
+    public void setAvgLetters(String text){
+        avgLetters = 0;
+    }
+    
+    public Double getAvgLetters(){
+        
+        return avgLetters;
+    }
+    
+    public void setAvgSentence(String text){
+        avgSentence = 0;
+    }
+    
+    public double getAvgSentence(){
+        
+        return avgSentence;
+    }
+}
