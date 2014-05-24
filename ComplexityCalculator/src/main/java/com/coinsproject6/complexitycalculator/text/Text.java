2bb3c87b13bfff66af20ea1450ecc951b7518aac
tobeflow <tobiasluebke@gmx.de>
2014-05-24 02:43:48 +0200

    google "did you mean?"-autosuggestion now integrated in Text.java - class in this version, all found hashtags are automatically replaced by their corresponding google autocorrect-suggestion

--- ComplexityCalculator/src/main/java/com/coinsproject6/complexitycalculator/text/Text.java
+++ ComplexityCalculator/src/main/java/com/coinsproject6/complexitycalculator/text/Text.java
@@ -5,6 +5,13 @@
  */
 package com.coinsproject6.complexitycalculator.text;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.zip.GZIPInputStream;
+
 /**
  * DRAFT - Erster Entwurf, es sind noch viele Anpassungen nötig!! Komplexe
  * Datenstruktur, die den Text mit allen nötigen Informationen repräsentiert.
@@ -128,7 +135,7 @@ public class Text {
         this.text = rawText;
         clearHyperlinks();
         clearReferences();
-        removeHashSymbols();
+        replaceHashesWithGoogleSuggest();
      
     }
     
@@ -136,6 +143,34 @@ public class Text {
     private void removeHashSymbols () {
         
         text = text.replaceAll("#", "");    
+    }
+    
+    public void replaceHashesWithGoogleSuggest() {
+        
+       int hashtagStart;
+       int hashtagEnd;
+       String hashtag;
+       String googleSuggest;
+       
+       while ((hashtagStart=text.indexOf("#")) > -1)
+       {
+           
+           hashtagEnd = hashtagStart + 1;
+           
+           while (Character.isLetterOrDigit(text.charAt(hashtagEnd))) {
+               hashtagEnd++;
+           }
+           
+           hashtag = text.substring(hashtagStart+1, hashtagEnd);
+
+          
+           googleSuggest = this.didYouMean(hashtag);
+           System.out.println("Google Suggest for #" + hashtag + " -> " + googleSuggest);
+           
+           text = text.replaceAll("#" + hashtag, googleSuggest);
+           
+       }
+       
     }
     
     
@@ -197,5 +232,58 @@ public class Text {
         }
         
         this.text = clearedText;
+    }
+    
+    
+  
+    private String didYouMean(String s){
+
+        String word = s;
+        String url = "http://www.google.co.in/search?hl=en&q="+URLEncoder.encode(s);
+        String html = executeGet(url,"www.google.co.in",'i');
+
+        int pq = html.indexOf("\"pq\":");
+        if(pq >= 0){
+            html = html.substring(pq + 6);
+            int endPQ = html.indexOf("\"");
+            if(endPQ > 0) {
+                html = html.substring(0,endPQ);
+            }
+            html = html.trim(); 
+        }
+        else { 
+            return "No results";
+        }
+        return html;
+    }
+
+
+    public String executeGet(String targetURL,String host,char ch){
+        URL url;
+        HttpURLConnection connection=null;
+        try{
+        url=new URL(targetURL);
+        connection=(HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Host",host);
+        connection.setRequestProperty("Accept-Encoding", "gzip,deflate,sdch");
+        connection.setRequestProperty("Accept-Language","en-US,en;q=0.8");
+        if(ch=='c') connection.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5");
+        if(ch=='i') connection.setRequestProperty("User-Agent","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0; InfoPath.2; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; ShopperReports 3.1.22.0; SRS_IT_E879047EB0765B5336AF90)");
+        connection.setUseCaches (false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        GZIPInputStream gzis=new GZIPInputStream(connection.getInputStream());
+        InputStreamReader reader=new InputStreamReader(gzis);
+        BufferedReader in=new BufferedReader(reader);
+        String line;
+        StringBuffer response=new StringBuffer();
+        while((line=in.readLine())!=null) {
+        response.append(line);
+        response.append('\r');
+        }
+        in.close();
+        return response.toString();
+        } catch (Exception e) {e.printStackTrace();return null;}
     }
 }
